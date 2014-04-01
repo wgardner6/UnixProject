36f463518ee38f51d2c55717ded9f41e2cfe2fef
wgardner6 <wgardner6@gmail.com>
2014-04-01 12:07:01 -0500

    Adding ability to read http by another program in NewsLinks.sh

--- Twitter/Project.sh
+++ Twitter/Project.sh
@@ -1,22 +1,38 @@
 #Locates user
+echo Calling GeoLoc
 bash GeoLoc.sh
 #Calls the API to get closest supported WOEID
-php callAPI.php LatLong.txt #'trends/closest' 'lat' '37.781157' 'long' '-122.4006128'
+echo Calling API with LatLong
+php callAPI.php LatLong.txt 
 #Formats woeid for use by trends/place
+echo Calling woeid.sh
 bash woeid.sh > woeid.txt
 #Call Twitter API to determine the trending words for a specific woeid
+echo Calling API with woeid
 php callAPI.php woeid.txt
 #Saves all the trends into a text file
+echo Calling trends to save as txt
 bash trends.sh > trends.txt
 #Output the trending data for specific place
+echo Sending trends to command line
 bash formatTrends.sh
 #Saves all the trends into appropriate wget calls
+echo Calling googleNews
 bash googleNews.sh
 #Calls wget on all the trend searches
+echo Calling wgetsGoogle
 bash wgetsGoogle.sh
 #Aggregates and formats all the trends to be Googled for
+echo Calling googleLinks
 bash googleLinks.sh
 #Has all the links to search google for
-bash LinkAggregator.sh
+echo Calling LinkAggregator
+#bash LinkAggregator.sh
+#Removing weird #s in LinkAggregator
+echo 'Removing #'
+cat LinkAggregator.txt | sed 's/#//g' > NewsLinks.sh
+#Searches google for newslinks relating to trending topics
+echo Calling NewsLinks
+bash NewsLinks.sh
 mv News\ for\ * NewsLinks/
 
--- Twitter/googleNews.sh
+++ Twitter/googleNews.sh
@@ -1,3 +1,4 @@
 #Creates the wget calls for the trends 
-cat place.txt | egrep 'name' | egrep -o '> .*$' | sed 's/> //g' | sed 's/\(.*$\)/wget \"https:\/\/news.google.com\/?q=\1\" -O \"Trends\/\1.txt\" -o \/dev\/null/' > wgetsGoogle.sh
+cat place.txt | egrep 'name' | egrep -o '> .*$' | sed 's/[>| |#]*\(.*\)/\1/g' | sed 's/\(.*$\)/wget \"https:\/\/news.google.com\/?q=\1\" -O \"Trends\/\1.txt\" -o \/dev\/null/' > wgetsGoogle.sh
 #cat $1 | sed 's/div>/div>\n/g' | egrep -o 'href="http[^\"]*' | egrep -viw 'google|youtube|favicon|googlenewsblog' 
+
