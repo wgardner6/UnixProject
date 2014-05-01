18aa79ed8e404a2dbfd6077445cafde3683a1551
wgardner6 <wgardner6@gmail.com>
2014-04-30 23:55:26 -0500

    Adding example config file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Goal is to find local Twitter trends and correlate those trending keywords with news articles.
+Need to create a directory twitterTokens that has a file called config.php that holds the API tokens.
 To run simply call makefile.
---
+++ Twitter/twitterTokens/config-example.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_TOKEN','');
+define('OAUTH_TOKEN_SECRET','');
+
+
+define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
